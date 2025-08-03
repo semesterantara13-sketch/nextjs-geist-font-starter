65f086d44fc57c5d9e3d28e85a7d8fbf0a032a64
semesterantara13-sketch <semesterantara13@gmail.com>
2025-08-03 23:45:06 +0700

    Add .next/static/chunks/src_app_layout_tsx_c0237562._.js 1754239505196

---
+++ .next/static/chunks/src_app_layout_tsx_c0237562._.js
@@ -0,0 +1,9 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__9a168d68._.css",
+  "static/chunks/node_modules_d4065aff._.js",
+  "static/chunks/src_19f8b5f2._.js"
+],
+    source: "dynamic"
+});
